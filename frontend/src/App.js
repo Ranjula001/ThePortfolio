47fbf18118f3d24f773eb01b4b64dd2c0a978322
Ranjula001 <it21011016@my.sliit.lk>
2023-06-30 23:21:01 +0530

    made some important stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import './App.scss';
+
+import { About, Footer, Header, Imonials} from './container';
+
+import {Navbar} from './components';
+
 const App = () => {
     return(
-        <div>App</div>
+        <div className='app'>
+            <Navbar/>
+            <Header/>
+            <About/>
+            <Imonials/>
+            <Footer/>
+        </div>
     )
 }
 export default App
