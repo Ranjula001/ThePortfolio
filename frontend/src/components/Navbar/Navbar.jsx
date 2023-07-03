64e94681c2545c5d0518d8471e686f8446fe9018
Ranjula001 <it21011016@my.sliit.lk>
2023-07-03 19:47:17 +0530

    navbar aligned

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,19 @@ import {images} from '../../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="Mylogo" />
+      </div>
+      <ul className="app__navbar-list">
+        {['Home', 'About', 'Skills', 'Contacts'].map( (list) => (
+          <li className="app__flex p-text" key={`link-${list}`}>
+            <div />
+            <a href={`#${list}`}>{list}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
